487a2d7350949a915151bcf9e16e92bacbe2bcac
xyil <44515243+xyil@users.noreply.github.com>
2022-02-04 09:42:08 -0500

    update println

--- hcl/src/linkedlist/MainList.java
+++ hcl/src/linkedlist/MainList.java
@@ -10,24 +10,31 @@ public class MainList{
 		seasons.add("Fall");
 		seasons.add("Winter");
 		
-		// Use removeFirst()
-		seasons.removeFirst();
-		
-		// Use addLast()
-		seasons.addLast("Spring");
-		
-		// Use removeLast()
-		seasons.removeLast();
-		
-		// Use add.First()
-		seasons.addFirst("Spring");
+		System.out.println("Initial list: " + seasons);
 		
 		// Use getFirst()
 		seasons.getFirst();
+		System.out.println("First list item " + seasons);
 		
 		// Use getLast()
 		seasons.getLast();
+		System.out.println("Last list item " + seasons);
 		
-		System.out.println(seasons);
+		// Use removeFirst()
+		seasons.removeFirst();
+		System.out.println("Remove first list: " + seasons);
+		
+		// Use addLast()
+		seasons.addLast("Spring");
+		System.out.println("addLast list: " + seasons);
+		
+		// Use removeLast()
+		seasons.removeLast();
+		System.out.println("Remove last list: " + seasons);
+		
+		// Use add.First()
+		seasons.addFirst("Spring");
+		System.out.println("Add first list: " + seasons);
+		
 	}
 }
